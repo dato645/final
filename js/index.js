d65260575214c32fba5dea3ec444a04596cf5f15
d <davitnazarov@gmail.com>
2023-04-14 20:59:42 +0400

    final project

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// var menuJs = document.getElementById("menuJs");
+// var olOn = document.getElementById("olOn");
+
+// burgerBtn.addEventListener("click", () => {
+//     // navEl.classList.toggle("
+//     // navEl.className = "show";
+//     olOn.classList.toggle("show"); 
+// });
